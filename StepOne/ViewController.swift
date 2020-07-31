f0a1aa3d75a4c58c21efa82b1ead39422bc4f59a
David Arebuwa <davidarebuwa@gmail.com>
2020-08-01 00:02:37 +0100

    first commit

---
+++ StepOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StepOne
+//
+//  Created by David Arebuwa on 31/07/2020.
+//  Copyright © 2020 David Arebuwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
